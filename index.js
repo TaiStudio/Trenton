9473e766c31441d787701e8a800fde6aa277f768
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2022-10-04 20:35:46 +0200

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+
+
+    //CAROUSSEL
+    carrousel();
+    setInterval(() => {
+        carrousel();
+    }, 5000);
+
+});
+
+
+z
+function carrousel(){
+    $('.carrousel').each((index, element) => {
+        let current = 0;
+        console.log(element);
+        $(`${element} img`).each((i, el) => {
+            if($(el).hasClass('hide') == false){
+                $(el).addClass('hide');
+            }
+            if($(el).hasClass('hide') == true){
+                $(el).removeClass('hide');
+            }
+        });
+    });
+}
