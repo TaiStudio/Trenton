239a836494da8cca3f6436cd28f145cbdbc32229
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-01-30 04:57:24 +0100

    Fix: code

--- template/default/component/code.js
+++ template/default/component/code.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import JSONPretty from 'react-json-pretty';
 
-function JSONBlock({ jsonData }) {
-    return (
-        <JSONPretty id="json-pretty" data={jsonData} ></JSONPretty>
-    );
+function JSONBlock({ data }) {
+    switch(data.lang){
+        case 'json':
+            return (
+                <JSONPretty id="json-pretty" data={JSON.parse(data.code)} ></JSONPretty>
+            );
+        default:
+            return (
+                <JSONPretty id="json-pretty" data={data.code} ></JSONPretty>
+            );
+    }
 }
 
 export default JSONBlock;
